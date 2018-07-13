f235123a91a866848b3e038c499a8e27ea5335c3
= <herlon214@gmail.com>
2018-07-12 23:41:06 -0400

    Button accept only title

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,16 +1,19 @@
-import React from 'react';
-import { Button, withStyles } from '@material-ui/core';
+// Libs
+import React from 'react'
+import { Button, withStyles } from '@material-ui/core'
 
 const styles = theme => ({
   button: {
-    margin: theme.spacing.unit ,
-  },
-});
+    margin: theme.spacing.unit
+  }
+})
 
 const ButtonComponent = ({ classes, title, icon, color, handler = () => {} }) => {
   return (
     <Button 
-      variant="fab" className={classes.button} color={color}
+      variant='contained'
+      className={classes.button}
+      color={color}
       onClick={() => handler()}>
       <i className='material-icons'> {icon} </i>
       {title}
